e4df4f47ea9922bd913bb973ee941c23286b2069
Code Lines <codelines@Home.local>
2022-09-06 01:36:06 -0400

    landig page: only missing some styling

--- wifi-power-cafe/main.py
+++ wifi-power-cafe/main.py
@@ -4,6 +4,10 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_sqlalchemy import SQLAlchemy
 from flask_bootstrap import Bootstrap
 from flask_login import current_user, login_required, UserMixin, login_user, LoginManager, logout_user
+from forms import *
+from utils import *
+from datetime import datetime
+
 
 
 
@@ -17,14 +21,15 @@ app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 
-
+# ---------------------------------- Database Tables Set up ------------------------------------ #
 
 class Cafe(db.Model):
-    __tablename__ = "cafes"
+    __tablename__ = "cafe"
     id = db.Column(db.Integer, primary_key=True)
     name = db.Column(db.String, nullable=False)
-    url = db.Column(db.String, nullable=False)
-    img = db.Column(db.String, nullable = False)
+    map_url = db.Column(db.String, nullable=False)
+    img_url = db.Column(db.String, nullable = False)
+    location = db.Column(db.String(100), nullable=False)
     opening_hours = db.Column(db.String, nullable=False)
     closing_hours = db.Column(db.String, nullable=False)
     wifi_rating = db.Column(db.String, nullable=False)
@@ -39,4 +44,18 @@ class User(UserMixin, db.Model):
     email = db.Column(db.String(100), nullable=False)
     password = db.Column(db.String(100), nullable=False)
 
+
+YEAR = datetime.today().strftime('%Y')
+print(YEAR)
     
+@app.route('/')
+def home():
+    return render_template('index.html', year=YEAR)
+
+@app.route('/cafes')
+def cafes():
+    return render_template('cafes.html')
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
